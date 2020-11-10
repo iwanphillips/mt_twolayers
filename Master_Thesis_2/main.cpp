cc1f95b0c529cd9ff109428fccb8012995abebac
Iwan Phillips <iwanguitar@hotmail.co.uk>
2020-11-10 18:25:48 +0100

    Initial Commit

---
+++ Master_Thesis_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Master_Thesis_2
+//
+//  Created by Iwan Phillips on 10.11.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
